c5c7342545b2407b935f26afbcd69d3ad6b5701b
Willians Martins <professorwillians@gmail.com>
2022-01-11 17:37:20 -0300

    add novas views

--- nodejs/ProjetoEventos/Nodejs/nodeEventos/controllers/eventos.js
+++ nodejs/ProjetoEventos/Nodejs/nodeEventos/controllers/eventos.js
@@ -4,6 +4,21 @@ module.exports = function (app) {
             var usuario = request.session.usuario,
                 params = { usuario: usuario };
             response.render('eventos/menu', params);
+        },
+        cadastroUsuario: function (request, response) {
+            var usuario = request.session.usuario,
+                params = { usuario: usuario };
+            response.render('eventos/cadUsuario', params);
+        },
+        cadastroEvento: function (request, response) {
+            var usuario = request.session.usuario,
+                params = { usuario: usuario };
+            response.render('eventos/cadEvento', params);
+        },
+        listaEventos: function (request, response) {
+            var usuario = request.session.usuario,
+                params = { usuario: usuario };
+            response.render('eventos/listaEventos', params);
         }
     };
     return EventosController;
--- nodejs/ProjetoEventos/Nodejs/nodeEventos/routes/eventos.js
+++ nodejs/ProjetoEventos/Nodejs/nodeEventos/routes/eventos.js
@@ -1,4 +1,8 @@
 module.exports = function (app) {
     var eventos = app.controllers.eventos;
     app.get('/menu', eventos.menu);
+
+    app.get('/cadUsuario', eventos.cadastroUsuario);
+    app.get('/cadEvento', eventos.cadastroEvento);
+    app.get('/listaEventos', eventos.listaEventos);
 };
