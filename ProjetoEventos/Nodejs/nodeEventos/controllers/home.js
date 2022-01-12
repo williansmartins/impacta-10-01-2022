a9411738730a8a285715e001b8bfea5b37921d37
Willians Martins <professorwillians@gmail.com>
2022-01-12 18:46:58 -0300

    tirando mock de usuario

--- ProjetoEventos/Nodejs/nodeEventos/controllers/home.js
+++ ProjetoEventos/Nodejs/nodeEventos/controllers/home.js
@@ -7,32 +7,36 @@ module.exports = function (app) {
             //definindo um novo usuario
             var nome = "usuario novo";
             var email = "email@teste.com";
-            var usuario = { "nome": nome, "email": email};
+            var usuario = { "nome": nome, "email": email };
 
             //definindo a resposta
             var resultado;
 
-            Usuario.create(usuario, function(erro, usuario){
-                if(erro){
+            Usuario.create(usuario, function (erro, usuario) {
+                if (erro) {
                     resultado = "Ocorreu um erro ao incluir usuário.";
-                }else{
+                } else {
                     resultado = "Usuário incluído com sucesso!";
                 }
             });
 
-            res.render('home/index', { titulo: "Express", resultado: resultado});
+            res.render('home/index', { titulo: "Express", resultado: resultado });
         },
         login: function (request, response) {
             var nome = request.body.usuario.nome;
             var senha = request.body.usuario.senha;
-            if (nome == 'admin' && senha == 'admin') {
-                var usuario = request.body.usuario;
-                request.session.usuario = usuario;
-                response.redirect('/menu');
-            }
-            else {
-                response.redirect('/');
-            }
+
+            var query = { 'nome': nome, 'senha': senha };
+            Usuario.findOne(query).select('nome senha')
+                .exec(function (erro, usuario) {
+                    if (erro) {
+                        response.redirect('/');
+                    }
+                    else {
+                        request.session.usuario = usuario;
+                        response.redirect('/menu');
+                    }
+                });
         },
         logout: function (request, response) {
             request.session.destroy();
@@ -42,8 +46,21 @@ module.exports = function (app) {
             var nome = request.body.usuario.nome;
             var senha = request.body.usuario.senha;
             var confirma = request.body.usuario.confirma;
-            //código a ser implementado
-            response.redirect('/menu');
+
+            if ((senha != confirma) || nome.trim().length == 0) {
+                response.redirect('/');
+            }
+            else {
+                var usuario = request.body.usuario;
+                Usuario.create(usuario, function (erro, usuario) {
+                    if (erro) {
+                        response.redirect('/');
+                    }
+                    else {
+                        response.redirect('/menu');
+                    }
+                });
+            }
         }
 
     };
