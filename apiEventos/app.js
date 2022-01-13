08ae2988a3e244b81c933a0feba893de4ef83ead
Willians Martins <professorwillians@gmail.com>
2022-01-13 19:13:32 -0300

    inicio do web service

--- apiEventos/app.js
+++ apiEventos/app.js
@@ -1,41 +1,146 @@
-var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+var load = require('express-load');
 var app = express();
+var bodyParser = require('body-parser');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+var mongoose = require('mongoose');
+global.db = mongoose.connect('mongodb://127.0.0.1:27017/neventos');
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+load('models').into(app);
+var Evento = app.models.eventos;
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.listen(3000, function () {
+  console.log('ok');
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+var Evento = app.models.eventos;
+var Pagamento = app.models.pagamentos;
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.listen('3200', function () {
+  console.log('Servidor iniciado!');
+})
+
+app.get('/', function (request, response) {
+  response.send('Servidor no ar');
 });
 
-module.exports = app;
+app.get('/eventos', function (request, response) {
+  Evento.find(function (erro, eventos) {
+    console.info(eventos);
+    if (erro) {
+      response.json(erro);
+    }
+    else {
+      response.json(eventos);
+    }
+  });
+});
+
+app.get('/eventos/:id', function (request, response) {
+  var id = request.params.id;
+  Evento.findById(id, function (erro, evento) {
+    if (erro) {
+      response.json(erro);
+    }
+    else {
+      response.json(evento);
+    }
+  });
+});
+
+app.post('/eventos', function (request, response) {
+  var descricao = request.body.descricao;
+  var data = request.body.data;
+  var preco = request.body.preco;
+  var evento = {
+    'descricao': descricao, 'data': data,
+    'preco': preco
+  };
+  Evento.create(evento, function (erro, evento) {
+    if (erro) {
+      response.json(erro);
+    }
+
+    else {
+      response.json(evento);
+    }
+  });
+});
+
+app.put('/eventos/:id', function (request, response) {
+  var id = request.params.id;
+
+  Evento.findById(id, function (erro, evento) {
+    if (erro) {
+      response.json(erro);
+    }
+    else {
+      var evento_upd = evento;
+      evento_upd.descricao = request.body.descricao;
+      evento_upd.data = request.body.data;
+      evento_upd.preco = request.body.preco;
+
+      evento_upd.save(function (erro, evento) {
+        if (erro) {
+          response.json(erro);
+        }
+        else {
+          response.json(evento);
+        }
+      });
+    }
+  });
+});
+
+app.delete('/eventos/:id', function (request, response) {
+  var id = request.params.id;
+
+
+  Evento.findById(id, function (erro, evento) {
+    if (erro) {
+      response.json(erro);
+    } else {
+      // response.json(evento);
+      Evento.deleteOne(evento, function (erro, evento) {
+        if (erro) {
+          response.json(erro);
+        }
+        else {
+          response.send('removido');
+        }
+      });
+    }
+  });
+});
+
+app.get('/pagamentos', function (request, response) {
+  Pagamento.find(function (erro, pagamento) {
+    if (erro) {
+      response.json(erro);
+    }
+    else {
+      response.json(pagamento);
+    }
+  });
+});
+
+app.post('/pagamentos', function (request, response) {
+  var evento = request.body.evento;
+  var preco = request.body.preco;
+  var numcartao = request.body.numcartao; var cvv = request.body.cvv;
+  var pagamento = {
+    'evento': evento, 'preco': preco, 'numcartao': numcartao, 'cvv': cvv
+  };
+
+  Pagamento.create(pagamento, function (erro, pagto) {
+    if (erro) {
+      response.json(erro);
+    }
+    else {
+      response.json(pagto);
+    }
+  });
+});
