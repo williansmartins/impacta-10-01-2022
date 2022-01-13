8bbbfaa4d4d6c2aa7ad1ee4a289e7e0450f2b8ec
Willians Martins <professorwillians@gmail.com>
2022-01-13 19:54:02 -0300

    criacao do model

--- apiRest/app.js
+++ apiRest/app.js
@@ -1,41 +1,30 @@
-var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var load = require('express-load');
 
 var app = express();
+var bodyParser = require('body-parser');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+var express = require('express');
+var load = require('express-load');
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+var app = express();
+var bodyParser = require('body-parser');
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/contatos');
+global.db = mongoose;
+
+load('models').into(app);
+
+// var Contato = app.models.contato;
+
+
+app.listen(3000, function () {
+	console.log('ok');
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
