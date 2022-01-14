00ee2cf4f382111c603edb50b51f90fb69f188d1
Willians Martins <professorwillians@gmail.com>
2022-01-14 12:45:07 -0300

    12

--- apiEventos/app.js
+++ apiEventos/app.js
@@ -39,10 +39,62 @@ app.get('/eventos/:id', function (request, response) {
   });
 });
 app.post('/eventos', function (request, response) {
+  var descricao = request.body.descricao;
+  var data = request.body.data;
+  var preco = request.body.preco;
+  var evento = {
+    'descricao': descricao,
+    'data': data,
+    'preco': preco
+  };
+  Evento.create(evento, function (erro, evento) {
+    if (erro) {
+      response.json(erro);
+    }
+    else {
+      response.json(evento);
+    }
+  });
 });
 app.put('/eventos/:id', function (request, response) {
+  var id = request.params.id;
+  Evento.findById(id, function (erro, evento) {
+    if (erro) {
+      response.json(erro);
+    }
+    else {
+      var evento_upd = evento;
+      evento_upd.descricao = request.body.descricao;
+      evento_upd.data = request.body.data;
+      evento_upd.preco = request.body.preco;
+      evento_upd.save(function (erro, evento) {
+        if (erro) {
+          response.json(erro);
+        }
+        else {
+          response.json(evento);
+        }
+      });
+      response.json(evento);
+    }
+  });
 });
 app.delete('/eventos/:id', function (request, response) {
+  var id = request.params.id;
+  Evento.findById(id, function (erro, evento) {
+    if (erro) {
+      response.json(erro);
+    } else {
+      Evento.remove(evento, function (erro, evento) {
+        if (erro) {
+          response.json(erro);
+        }
+        else {
+          response.send('removido');
+        }
+      });
+    }
+  });
 });
 
 app.listen(3200, function () {
