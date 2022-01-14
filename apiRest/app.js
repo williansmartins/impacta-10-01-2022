b6914b1a2baf3793d616852cf2862c894355fe4a
Willians Martins <professorwillians@gmail.com>
2022-01-13 22:19:19 -0300

    add metodos put, post, delete

--- apiRest/app.js
+++ apiRest/app.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/contatos');
+mongoose.connect('mongodb://127.0.0.1:27017/contatos');
 global.db = mongoose;
 
 load('models')
--- apiRest/routes/contato.js
+++ apiRest/routes/contato.js
@@ -1,8 +1,100 @@
 module.exports = function (app) {
 
-  app.get('/contato', function(requisicao, resposta){
-    resposta.json("acessando todos os contatos");
-  });
+    var Contato = app.models.contato;
 
-  
-};
+    app.get('/', function (request, response) {
+        response.send('Servidor no ar');
+    })
+
+    app.get('/contatos', function (request, response) { 
+        Contato.find(function (erro, contato) {
+            if (erro) {
+                console.info("Erro: " + erro);
+                response.send(erro);
+            } else {
+                console.info(contato);
+                response.send(contato);
+            }
+        });
+    })
+
+    app.get('/contatos/:id', function (request, response) {
+        var id = request.params.id;
+
+        Contato.findById(id, function (erro, contato) {
+            if (erro) {
+                console.info("Erro: " + erro);
+                response.send(erro);
+            } else {
+                console.info(contato);
+                response.send(contato);
+            }
+        });
+
+    })
+
+    app.post('/contatos', function (request, response) {
+        var cpf = request.body.cpf;
+        var nome = request.body.nome;
+        var telefone = request.body.telefone;
+
+        var contato = {
+            'cpf': cpf,
+            'nome': nome,
+            'telefone': telefone
+        }
+
+        Contato.create(contato, function (erro, contato) {
+            if (erro) {
+                console.info("Erro: " + erro);
+                response.send(erro);
+            } else {
+                console.info(contato);
+                response.send(contato);
+            }
+        });
+
+    })
+
+    app.delete('/contatos/:id', function (request, response) {
+        var id = request.params.id;
+
+        console.info(">>>>>>" + id);
+
+        Contato.remove({ "_id": id }, function (erro, contato) {
+            if (erro) {
+                console.info("Erro: " + erro);
+                response.send(erro);
+            } else {
+                console.info(contato);
+                response.send('removido');
+            }
+        });
+
+    })
+
+    app.put('/contatos/:id', function (request, response) {
+        var id = request.params.id;
+
+        var cpf = request.body.cpf;
+        var nome = request.body.nome;
+        var telefone = request.body.telefone;
+
+        Contato.findById(id, function (erro, contato) {
+
+            var contatoUpdated = contato;
+            contatoUpdated.cpf = cpf;
+            contatoUpdated.nome = nome;
+            contatoUpdated.telefone = telefone;
+
+            contatoUpdated.save(contatoUpdated, function (erro, contato) {
+                if (erro) {
+                    response.send(erro);
+                } else {
+                    response.send(contato);
+                }
+            });
+
+        });
+    });
+}
