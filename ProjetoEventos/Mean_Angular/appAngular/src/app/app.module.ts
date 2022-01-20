182f14c1c0dc3ce0087c7780370e64e5c3b65da2
Willians Martins <professorwillians@gmail.com>
2022-01-20 14:31:31 -0300

    criando componente

--- ProjetoEventos/Mean_Angular/appAngular/src/app/app.module.ts
+++ ProjetoEventos/Mean_Angular/appAngular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventosComponent } from './eventos/eventos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventosComponent
   ],
   imports: [
     BrowserModule,
---
+++ ProjetoEventos/Mean_Angular/appAngular/src/app/eventos/eventos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.less']
+})
+export class EventosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
